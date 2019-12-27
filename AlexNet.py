0ed24e81b965c1d5a73c057da1c1ba04cd560e94
Heechul90 <heechul4296@gmail.com>
2019-12-27 13:43:17 +0900

    I'm studying MXnet

--- AlexNet.py
+++ AlexNet.py
@@ -2,6 +2,15 @@ import mxnet as mx
 from mxnet import gluon, autograd, ndarray
 
 
+# 데이터 가져오기
+data = mx.test_utils.get_mnist()
+
+# 학습 데이터 설정 및 사진 재구성
+train_data = data['train_data'].reshape((-1, 784))
+train_label = data['train_label']
+
+
+
 net = gluon.nn.Sequential()
 with net.name_scope():
     # 은닉층1
@@ -24,31 +33,3 @@ with net.name_scope():
     net.add(gluon.nn.Dense(4096, activation='relu'))
     # 출력층
     net.add(gluon.nn.Dense(1000, activation='softmax'))
-
-
-def alexnet(width, height, lr):
-    network = input_data(shape=[None, width, height, 1], name='input')
-    network = conv_2d(network, 96, 11, strides=4, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    network = conv_2d(network, 256, 5, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 256, 3, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-    network = fully_connected(network, 3, activation='softmax')
-    network = regression(network, optimizer='momentum',
-                         loss='categorical_crossentropy',
-                         learning_rate=lr, name='targets')
-
-    model = tflearn.DNN(network, checkpoint_path='model_alexnet',
-                        max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
-
-    return model
