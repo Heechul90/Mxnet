00918f2b0776d380d21ba5040791f3e1e69bdf0b
Heechul90 <heechul4296@gmail.com>
2020-01-15 18:34:24 +0900

    mxnet

--- Study/Ch05/Chapter05.py
+++ Study/Ch05/Chapter05.py
@@ -13,7 +13,7 @@ mnist_test = mx.gluon.data.vision.MNIST(train = False, transform = True)
 ycount = nd.ones(shape = (10))
 xcount = nd.ones(shape = (784, 10))
 
-for data. label in mnist_train:
+for data, label in mnist_train:
     x = data.reshape((784, ))
     y = int(label)
     ycount[y] += 1
